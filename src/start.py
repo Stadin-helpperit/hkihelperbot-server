4fb8fb60934d28036fe592c9eb63b462d2850117
henettaja <henri.vaisane@gmail.com>
2020-11-09 13:57:19 +0200

    Ready to deploy to heroku

--- src/start.py
+++ src/start.py
@@ -1,7 +1,13 @@
+from dotenv import load_dotenv
 from handlers import updater
 import logging
+import os
 
 # --- RUN THIS TO TURN THE BOT ON ---
+
+port = int(os.environ.get('PORT', 5000))
+load_dotenv()
+token = os.environ.get("API_TOKEN")
 
 
 def main():
@@ -9,7 +15,12 @@ def main():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
     # starts the polling of incoming messages
-    updater.start_polling()
+    # updater.start_polling()
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(port),
+                          url_path=token)
+    updater.bot.setWebhook('https://evening-everglades-42789.herokuapp.com/' + token)
 
 
 if __name__ == "__main__":
