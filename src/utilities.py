451173217df2285d2755e0b3aa9fdfdf317651b4
FeeliksKilpi <feelikskilpi@outlook.com>
2020-12-03 14:23:02 +0200

    Refactored keyboard tag markup to be dynamic

--- src/utilities.py
+++ src/utilities.py
@@ -1,9 +1,15 @@
 from datetime import datetime
-import pytz
+import pytz, json
 from telegram import InlineKeyboardButton
+from emoji import emojize
 
 
 # Helper functions to reformat date/time data
+
+# This function constructs the help message for the user
+with open("../tag_keyboard.json") as json_data:
+    tag_keyboard_data = json.load(json_data)
+
 
 def str_to_datetime(date_string):
     date_time_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -22,46 +28,25 @@ def datetime_to_str(date_time_object):
     return date_string
 
 
-def create_tag_keyboard_markup(data_type):
-    # List of popular tags that is used when the /search-command is invoked without a parameter
-    if data_type == 'p':
-        popular_tags = [['accomodation', '\U0001F3E8 Acommodation'],
-                        ['sights & attractions', '\U0001F5FC Sights'],
-                        ['nature & sports', '\U0001F332\U000026BD Nature and sports'],
-                        ['museums & galleries', '\U0001F3A8 Museums and galleries'],
-                        ['restaurants & cafes', '\U0001F372 Restaurants and Cafés'],
-                        ['sauna & wellness', '\U0001F9D6 Wellbeing'],
-                        ['venues', '\U0001F3DB Event places'],
-                        ['services', '\U0001F689 Services'],
-                        ['bars & nightlife', '\U0001F37B Bars and nightlife'],
-                        ['work & study', '\U0001F4DA Work and study']]
-    else:
-        popular_tags = [['music', '\U0001F3B6 Music'],
-                        ['theatre', '\U0001F3AD Theatre'],
-                        ['culture', '\U0001F3A8 Culture'],
-                        ['sports', '\U000026BD Sports'],
-                        ['museums', '\U0001F3DB Museums'],
-                        ['nature', '\U0001F332 Nature'],
-                        ['food', '\U0001F372 Food'],
-                        ['families with children', '\U0001F46A Families'],
-                        ['workshops', '\U0001F6E0 Workshops']]
-
-    # some functionality to turn the list of tags into telegram keyboard markup
+# Function that creates buttons for tag keyboard by chosen data type, tag list and columns amount
+def create_buttons(tag_list, columns, data_type):
+    print(tag_list["tags"])
     tag_keyboard = []
     tag_keyboard_row = []
-    if data_type == 'p':
-        for tag in popular_tags:
-            new_button = InlineKeyboardButton(text=tag[1], callback_data=data_type + '_' + tag[0])
-            tag_keyboard_row.append(new_button)
-            if len(tag_keyboard_row) == 2:
-                tag_keyboard.append(tag_keyboard_row)
-                tag_keyboard_row = []
-    else:
-        for tag in popular_tags:
-            new_button = InlineKeyboardButton(text=tag[1], callback_data=data_type + '_' + tag[0])
-            tag_keyboard_row.append(new_button)
-            if len(tag_keyboard_row) == 3:
-                tag_keyboard.append(tag_keyboard_row)
-                tag_keyboard_row = []
-
+    for tag in tag_list["tags"]:
+        new_button = InlineKeyboardButton(text=(emojize(tag["tag"] + ' ' + tag["emoji"], use_aliases=True)), callback_data=data_type + '_' + tag["key"])
+        tag_keyboard_row.append(new_button)
+        if len(tag_keyboard_row) == columns:
+            tag_keyboard.append(tag_keyboard_row)
+            tag_keyboard_row = []
     return tag_keyboard
+
+
+# Function that creates tag keyboard markup by calling create_buttons
+def create_tag_keyboard_markup(data_type):
+    # p = places, t = events, a = activities
+    if data_type == 'p':
+        keyboard_markup = create_buttons(tag_keyboard_data[1], 2, data_type)
+    else:
+        keyboard_markup = create_buttons(tag_keyboard_data[0], 3, data_type)
+    return keyboard_markup
